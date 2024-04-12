c78369e865d2296a633d0b8fa046fda2d32ee89f
dgibson24 <dgibson10364@gmail.com>
2024-04-12 13:06:09 -0400

    done

--- main.js
+++
@@ -1,9 +0,0 @@
-let accordions = document.querySelectorAll(".accordion-wrapper .accordion");
-accordions.forEach((acco)=>{
-    acco.onclick = () =>{
-        accordions.forEach((subcontent)=>{
-            subcontent.classList.remove("active");
-        })
-        acco.classList.add("active");
-    };
-})
